1eb6b76ddcb5078a3cb6713291ee907d9b80fa56
Frederik91 <frederik.t91@gmail.com>
2015-08-12 22:12:41 +0200

    Fikset en bugg der DateTime krasjet pga. windows datosystem.

---
+++ ProjectCostEstimator/Model/DisciplineValues.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectCostEstimator.Model
+{
+    class DisciplineValues
+    {
+        public string Discipline { get; set; }
+        public string Chapter { get; set; }
+        public int Cost { get; set; }
+    }
+}
